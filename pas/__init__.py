e34bc824b597bc94e80203ffc2eff4afaf3e2b2b
Omar Al-Hinai <ohinai@gmail.com>
2014-05-09 17:47:06 -0500

    initial commit of repo setup by cookiecutter

---
+++ pas/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Omar Al-Hinai'
+__email__ = 'ohinai@gmail.com'
+__version__ = '0.1.0'
---
+++ tests/test_pas.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pas
+----------------------------------
+
+Tests for `pas` module.
+"""
+
+import unittest
+
+from pas import pas
+
+
+class TestPas(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
